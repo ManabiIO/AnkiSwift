8cbd203ed2a63fd8e8143315b220f32af869ca1b
Alex Ehlke <alex.ehlke@gmail.com>
2025-03-25 18:45:01 -0400

    wip

--- Sources/AnkiSwift/Actions/Model/FindModelsByName.swift
+++ Sources/AnkiSwift/Actions/Model/FindModelsByName.swift
@@ -1,56 +1,56 @@
 #if os(macOS)
 public struct Model: Decodable {
-    let id: Int64
-    let name: String
-    let type: Int
-    let mod: Int
-    let usn: Int
-    let sortf: Int
-    let did: Int?
-    let tmpls: [Template]
-    let flds: [Field]
-    let css: String
-    let latexPre: String
-    let latexPost: String
-    let latexsvg: Bool
-    let req: [[Model.RequiredField]]
-    let originalStockKind: Int
+    public let id: Int64
+    public let name: String
+    public let type: Int
+    public let mod: Int
+    public let usn: Int
+    public let sortf: Int
+    public let did: Int?
+    public let tmpls: [Template]
+    public let flds: [Field]
+    public let css: String
+    public let latexPre: String
+    public let latexPost: String
+    public let latexsvg: Bool
+    public let req: [[Model.RequiredField]]
+    public let originalStockKind: Int
     
-    struct Template: Decodable {
-        let name: String
-        let ord: Int
-        let qfmt: String
-        let afmt: String
-        let bqfmt: String
-        let bafmt: String
-        let did: Int?
-        let bfont: String
-        let bsize: Int
-        let id: Int64
+    public struct Template: Decodable {
+        public let name: String
+        public let ord: Int
+        public let qfmt: String
+        public let afmt: String
+        public let bqfmt: String
+        public let bafmt: String
+        public let did: Int?
+        public let bfont: String
+        public let bsize: Int
+        public let id: Int64
     }
     
-    struct Field: Decodable {
-        let name: String
-        let ord: Int
-        let sticky: Bool
-        let rtl: Bool
-        let font: String
-        let size: Int
-        let description: String
-        let plainText: Bool
-        let collapsed: Bool
-        let excludeFromSearch: Bool
-        let id: Int64
-        let tag: String?
-        let preventDeletion: Bool
+    public struct Field: Decodable {
+        public let name: String
+        public let ord: Int
+        public let sticky: Bool
+        public let rtl: Bool
+        public let font: String
+        public let size: Int
+        public let description: String
+        public let plainText: Bool
+        public let collapsed: Bool
+        public let excludeFromSearch: Bool
+        public let id: Int64
+        public let tag: String?
+        public let preventDeletion: Bool
     }
     
-    struct RequiredField: Decodable {
-        let index: Int
-        let requirement: String
-        let fieldIndexes: [Int]
+    public struct RequiredField: Decodable {
+        public let index: Int
+        public let requirement: String
+        public let fieldIndexes: [Int]
         
-        init(from decoder: Decoder) throws {
+        public init(from decoder: Decoder) throws {
             var container = try decoder.unkeyedContainer()
             index = try container.decode(Int.self)
             requirement = try container.decode(String.self)
